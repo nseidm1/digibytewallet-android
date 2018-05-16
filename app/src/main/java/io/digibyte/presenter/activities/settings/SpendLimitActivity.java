d7701cd0468a515d7c2b84181a23777acfd7ab44
Noah Seidman <noah.seidman@universalorlando.com>
2018-05-16 14:29:40 -0400

    Fingerprint spending limits theming.

--- app/src/main/java/io/digibyte/presenter/activities/settings/SpendLimitActivity.java
+++ app/src/main/java/io/digibyte/presenter/activities/settings/SpendLimitActivity.java
@@ -31,10 +31,6 @@ public class SpendLimitActivity extends BRActivity {
     List<Integer> items = new ArrayList<>();
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spend_limit);
