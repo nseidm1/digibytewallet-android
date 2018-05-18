4bbd51933678df52b0dd675b8472584c01ca794b
Noah <noah@noahseidman.com>
2018-05-17 22:57:39 -0400

    WriteDownActivity needs the up affordance to go to the BreadActivity particularly after just creating a new wallet. Fixed send fragment amount edit being overlayed by multi character iso.

--- app/src/main/java/io/digibyte/presenter/activities/intro/WriteDownActivity.java
+++ app/src/main/java/io/digibyte/presenter/activities/intro/WriteDownActivity.java
@@ -2,12 +2,14 @@ package io.digibyte.presenter.activities.intro;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import io.digibyte.R;
 import io.digibyte.databinding.ActivityWriteDownBinding;
 import io.digibyte.presenter.activities.callbacks.ActivityWriteDownCallback;
 import io.digibyte.presenter.activities.util.BRActivity;
 import io.digibyte.presenter.interfaces.BRAuthCompletion;
+import io.digibyte.tools.animation.BRAnimator;
 import io.digibyte.tools.security.AuthManager;
 import io.digibyte.tools.security.PostAuth;
 
@@ -37,5 +39,16 @@ public class WriteDownActivity extends BRActivity {
         binding.setCallback(callback);
         setupToolbar();
         setToolbarTitle(R.string.SecurityCenter_paperKeyTitle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.home:
+                BRAnimator.startBreadActivity(WriteDownActivity.this, false);
+                return true;
+            default:
+                return false;
+        }
     }
 }
